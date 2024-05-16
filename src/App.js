bb9b5cacd20b83b4fc5a9eadbe410dceb0dccf5a
Damitht <tenna_koon@yahoo.com>
2024-05-16 17:04:08 +1000

    GET POST PUT DELETE operations with JSON server and axios

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import BookCreate from "./components/BookCreate";
+import BookList from "./components/BookList";
+import axios from "axios";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function App(){
+const [books, setBooks] = useState([]);
+
+    const editBookById = (id, newTitle) => {
+        const updatedBooks = books.map((book) => {
+            if(book.id === id){
+                return {...book, title: newTitle};
+            }
+            return book;
+        });
+
+        setBooks(updatedBooks);
+    };
+
+    const createBook = async (title) =>{
+        // const updatedBooks = [
+        //     ...books,
+        //     {
+        // id:Math.round(Math.random() * 9999), 
+        //         title:title
+        //  }
+        // ];
+        // setBooks(updatedBooks);
+
+        const response = await axios.post('http://localhost:3001/books',{
+            title: title
+        });
+
+        const updatedBooks = [
+            ...books,
+            response.data
+        ];
+        setBooks(updatedBooks);
+    };
+
+    const deleteBookById = (id) => {
+        const deletedBooks = books.filter ((book) => {
+            return book.id !== id;
+        });
+
+        setBooks(deletedBooks);
+    };
+
+    return (
+        <div className="app">
+            <h1>Reading List</h1>
+            <BookList books={books} onDelete={deleteBookById} onEdit={editBookById}/>
+            <BookCreate onCreate={createBook}/>
+        </div>
+    );
 }
 
 export default App;
